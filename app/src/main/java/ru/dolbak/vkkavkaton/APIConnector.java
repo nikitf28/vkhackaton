d5afcc1cda51d1227bc67aad7ce546ef9353683c
Nikita Dolbak <nikitf28@yandex.ru>
2020-11-07 19:11:12 +0800

    New Methods

--- app/src/main/java/ru/dolbak/vkkavkaton/APIConnector.java
+++ app/src/main/java/ru/dolbak/vkkavkaton/APIConnector.java
@@ -53,7 +53,7 @@ public class APIConnector {
         MediaType mediaType = MediaType.parse("application/json");
         RequestBody body = RequestBody.create(mediaType, "{\"email\": \"" + email + "\", \"password\": \"" + pwd + "\"}");
         Request request = new Request.Builder()
-                .url(domain + "/authorize")
+                .url(domain + "/register")
                 .post(body)
                 .addHeader("content-type", "application/json")
                 .addHeader("accept", "application/json")
@@ -79,6 +79,67 @@ public class APIConnector {
         JSONObject jObject = new JSONObject(responseBody.string());
         return new String[] {"token", jObject.getString("token")};
     }
+
+    public static String[] profileMeGet(String token) throws Exception {
+        MediaType mediaType = MediaType.parse("application/json");
+        Request request = new Request.Builder()
+                .url(domain + "/profile/me")
+                .get()
+                .addHeader("content-type", "application/json")
+                .addHeader("accept", "application/json")
+                .addHeader("X-Auth-Token", token)
+                .build();
+        Log.d("Internet", request.toString());
+        Response response = client.newCall(request).execute();
+        if(response.code() != 200) {
+            ResponseBody responseBody  = response.body();
+            JSONObject jObject = new JSONObject(responseBody.string());
+            if (jObject.getString("Code").equals("PROFILE_NOT_READY")){
+                return new String[] {"ERROR", "PROFILE_NOT_READY"};
+            }
+            if (jObject.getString("Code").equals("INVALID_TOKEN")){
+                return new String[] {"ERROR", "INVALID_TOKEN"};
+            }
+            return new String[] {"ERROR", "unknown error"};
+        }
+        ResponseBody responseBody  = response.body();
+        // вроде бы такого быть не должно
+        if(responseBody  == null) {
+            return null;
+        }
+        JSONObject jObject = new JSONObject(responseBody.string());
+        return new String[] {"data", jObject.getString("FirstName"), jObject.getString("LastName"), jObject.getString("Age"),
+                jObject.getString("Bio"), jObject.getString("Pictures"), jObject.getString("Tags")};
+    }
+
+    public static String[] profileMePost(String token, String FirstName, String LastName, String BirthDate) throws Exception {
+        MediaType mediaType = MediaType.parse("application/json");
+        Request request = new Request.Builder()
+                .url(domain + "/profile/me")
+                .get()
+                .addHeader("content-type", "application/json")
+                .addHeader("accept", "application/json")
+                .addHeader("X-Auth-Token", token)
+                .build();
+        Log.d("Internet", request.toString());
+        Response response = client.newCall(request).execute();
+        if(response.code() != 200) {
+            ResponseBody responseBody  = response.body();
+            JSONObject jObject = new JSONObject(responseBody.string());
+            if (jObject.getString("Code").equals("INVALID_BIRTHDATE")){
+                return new String[] {"ERROR", "INVALID_BIRTHDATE"};
+            }
+            return new String[] {"ERROR", "unknown error"};
+        }
+        ResponseBody responseBody  = response.body();
+        // вроде бы такого быть не должно
+        if(responseBody  == null) {
+            return null;
+        }
+        JSONObject jObject = new JSONObject(responseBody.string());
+        return new String[] {"OK", ""};
+    }
+
 
 
 
